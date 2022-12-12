94cc0a1bab493a2d7d08aedf05ec7aed75ed735e
pleblira <91032628+pleblira@users.noreply.github.com>
2022-12-12 08:22:40 -0500

    removing period at end of tweet message when quot

--- main.py
+++ main.py
@@ -80,14 +80,11 @@ def get_stream(set):
                 tweet_message = json_response["data"]["text"]
                 tweet_message = remove_mentions_from_tweet_message(tweet_message)
                 tweet_message = "\""+tweet_message.upper()+"\""+"?!\n\noof, BIG OOF"
-            # if "#stackjoinadd" in json_response['data']['text'].lower():
-            #     print("found #stackjoinadd on tweet, activating stackjoin_add function")
-            #     if json_response_from_stackjoinadd != None:
-            #         store_stackjoin(json_response_from_stackjoinadd[0],json_response_from_stackjoinadd[1],json_response_from_stackjoinadd[2], json_response_from_stackjoinadd[3])
-            # else:
-            #     print("didn't find #stackjoinadd on tweet, so not activating the stackjoin_add function")
             throttle_list = create_throttle_list(throttle_time)
             # print(f"json dumps for get_stream: {json.dumps(json_response, indent=4, sort_keys=True)}")
+            # processing tweet message
+            if tweet_message[len(tweet_message)-1:len(tweet_message)] == ".":
+                tweet_message = tweet_message[:len(tweet_message)-1]
             # tweet_message = json_response["data"]["text"]
             # if tweet_message.strip()[0:len(tweet_message)-9] == (tweet_message.strip()[0:tweet_message.lower().find("fewbot21")-8]):
             #     tweet_message = "Few"
